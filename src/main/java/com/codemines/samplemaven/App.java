a5304f8cdac4782933ee7b136a5cb2c5d36a6491
Pankaj Verma <pnkjbhopal@gmail.com>
2018-06-12 13:01:38 +0530

    first commit

---
+++ src/main/java/com/codemines/samplemaven/App.java
@@ -0,0 +1,13 @@
+package com.codemines.samplemaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
